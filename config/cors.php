28c9cf44e369a34212e15bcb604fad2b9a6c2fe2
JorgeNunes <jlbnunes@live.com>
2024-05-30 17:25:39 -0300

    fix:cors resolved update laravel 11.09

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:9010', 'https://test.strategyanalytics.com.br/'],
+    'allowed_origins' => ['http://localhost:9010', 'https://test.strategyanalytics.com.br', '.strategyanalytics.com.br'],
 
     'allowed_origins_patterns' => [],
 
